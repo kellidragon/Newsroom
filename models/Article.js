eec2ce9b66b92b07f3f81e02a7bcd7c1a08d17b1
kellidragon <dragonkelli@gmail.com>
2019-07-19 13:47:47 -0700

    scraping complete

--- models/Article.js
+++ models/Article.js
@@ -1,24 +1,15 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 // Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
-    type: String,
-    required: true
+    type: String
   },
-  // `link` is required and of type String
   link: {
-    type: String,
-    required: true
+    type: String
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
--- server.js
+++ server.js
@@ -15,8 +15,6 @@ const PORT = 3000;
 const app = express();
 
 // Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -30,26 +28,21 @@ mongoose.connect((MONGODB_URI), { useNewUrlParser: true });;
 
 // Routes
 
-// A GET route for scraping the website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+  axios.get("https://news.google.com/").then(function(response) {
+    
     let $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
+    $("article h3").each(function(i, element) {
       let result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
+     // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children("a")
         .text();
       result.link = $(this)
         .children("a")
         .attr("href");
-
+        // console.log(result)
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
@@ -69,14 +62,11 @@ app.get("/scrape", function(req, res) {
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
@@ -121,3 +111,4 @@ app.post("/articles/:id", function(req, res) {
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
+
